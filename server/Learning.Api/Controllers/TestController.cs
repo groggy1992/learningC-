1d05a376923acd71dee213c252f8462f168084a9
keisukekasai <kkasai2017@gmail.com>
2019-07-02 23:34:13 +0900

    feat: create C#_project .net core

---
+++ server/Learning.Api/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learning.Api.Controllers
+{
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public String ReturnHelloWorld()
+        {
+            return "HellowWorld";
+        }
+    }
+}
